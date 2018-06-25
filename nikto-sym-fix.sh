d021577738e392335dda0d8c5a2e6aff95549f51
Ben 5h4 <benshaji888@gmail.com>
2018-06-25 11:33:40 +0530

    Update nikto-sym-fix.sh

--- nikto-sym-fix.sh
+++ nikto-sym-fix.sh
@@ -1,1 +1,8 @@
+#!/data/data/com.termux/files/usr/bin/bash
+#SOMEColors
+red='\033[1;31m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+reset='\033[0m'
+echo "$red"
 git clone https://github.com/bensh4/nikto-sym-fix/
