784b597d1ad13905709ba96f517741a0e404f4d8
Ben 5h4 <benshaji888@gmail.com>
2018-06-29 18:49:57 +0530

    Update nikto-sym-fix.sh

--- nikto-sym-fix.sh
+++ nikto-sym-fix.sh
@@ -5,4 +5,12 @@ yellow='\033[1;33m'
 blue='\033[1;34m'
 reset='\033[0m'
 echo "$red"
+echo "CLONING THE AUTOMATED SCRIPT FROM girhub.com/bensh4/nikto-sym-fix"
 git clone https://github.com/bensh4/nikto-sym-fix/
+sleep 2
+echo "
+    +-+-+-+-+
+    |D|O|N|E|.
+    +-+-+-+-+
+    "
+    echo "please run the script by typing 'bash nikto-sym-fix' "
